6ca7d61ab55fd4897cbb63a08a830fb5f1c4c586
Orguntay <67823328+Orguntay@users.noreply.github.com>
2021-01-31 00:05:16 +0300

    Add project files.

---
+++ Entities/Abstaract/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstaract
+{
+    // IEntity implament eden bir class bir veri tabanı tablosudur
+    public interface IEntity
+    {
+    }
+}
